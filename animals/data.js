d9c083152a3e248b82717f37cee595f4eed31870
minzhang190 <minzhang190@gmail.com>
2020-04-20 03:20:47 +0000

    +animals

---
+++ animals/data.js
@@ -0,0 +1,132 @@
+var title = "Dòngwu";
+
+var data = [
+    {
+        "image": "assets/5e9cda08037c6b87b73250f1.png",
+        "sound1": "assets/5e9cda08037c6b87b73250f1.zh.female.mp3",
+        "sound2": "assets/5e9cda08037c6b87b73250f1.zh.male.mp3",
+        "sound3": "assets/5e9cda08037c6b87b73250f1.en.mp3",
+        "pinyin": "dòng wu",
+        "english": "animal"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f2.png",
+        "sound1": "assets/5e9cda09037c6b87b73250f2.zh.female.mp3",
+        "sound2": "assets/5e9cda09037c6b87b73250f2.zh.male.mp3",
+        "sound3": "assets/5e9cda09037c6b87b73250f2.en.mp3",
+        "pinyin": "niú",
+        "english": "cattle"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f3.png",
+        "sound1": "assets/5e9cda09037c6b87b73250f3.zh.female.mp3",
+        "sound2": "assets/5e9cda09037c6b87b73250f3.zh.male.mp3",
+        "sound3": "assets/5e9cda09037c6b87b73250f3.en.mp3",
+        "pinyin": "shān yáng",
+        "english": "goat"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f4.png",
+        "sound1": "assets/5e9cda09037c6b87b73250f4.zh.female.mp3",
+        "sound2": "assets/5e9cda09037c6b87b73250f4.zh.male.mp3",
+        "sound3": "assets/5e9cda09037c6b87b73250f4.en.mp3",
+        "pinyin": "mián yáng",
+        "english": "sheep"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4531.png",
+        "sound1": "assets/5e8dab8369c2516911ea4531.zh.female.mp3",
+        "sound2": "assets/5e8dab8369c2516911ea4531.zh.male.mp3",
+        "sound3": "assets/5e8dab8369c2516911ea4531.en.mp3",
+        "pinyin": "gǒu",
+        "english": "dog"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4530.png",
+        "sound1": "assets/5e8dab8369c2516911ea4530.zh.female.mp3",
+        "sound2": "assets/5e8dab8369c2516911ea4530.zh.male.mp3",
+        "sound3": "assets/5e8dab8369c2516911ea4530.en.mp3",
+        "pinyin": "māo",
+        "english": "cat"
+    },
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f5.png",
+        "sound1": "assets/5e9cda0b037c6b87b73250f5.zh.female.mp3",
+        "sound2": "assets/5e9cda0b037c6b87b73250f5.zh.male.mp3",
+        "sound3": "assets/5e9cda0b037c6b87b73250f5.en.mp3",
+        "pinyin": "gōng jī",
+        "english": "rooster"
+    },
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f6.png",
+        "sound1": "assets/5e9cda0b037c6b87b73250f6.zh.female.mp3",
+        "sound2": "assets/5e9cda0b037c6b87b73250f6.zh.male.mp3",
+        "sound3": "assets/5e9cda0b037c6b87b73250f6.en.mp3",
+        "pinyin": "mǔ jī",
+        "english": "hen"
+    },
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f7.png",
+        "sound1": "assets/5e9cda0b037c6b87b73250f7.zh.female.mp3",
+        "sound2": "assets/5e9cda0b037c6b87b73250f7.zh.male.mp3",
+        "sound3": "assets/5e9cda0b037c6b87b73250f7.en.mp3",
+        "pinyin": "xiǎo jī",
+        "english": "chick"
+    },
+    {
+        "image": "assets/5e9cda0c037c6b87b73250f8.png",
+        "sound1": "assets/5e9cda0c037c6b87b73250f8.zh.female.mp3",
+        "sound2": "assets/5e9cda0c037c6b87b73250f8.zh.male.mp3",
+        "sound3": "assets/5e9cda0c037c6b87b73250f8.en.mp3",
+        "pinyin": "huǒ jī",
+        "english": "turkey"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4532.png",
+        "sound1": "assets/5e8dab8369c2516911ea4532.zh.female.mp3",
+        "sound2": "assets/5e8dab8369c2516911ea4532.zh.male.mp3",
+        "sound3": "assets/5e8dab8369c2516911ea4532.en.mp3",
+        "pinyin": "yā zi",
+        "english": "duck"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250f9.png",
+        "sound1": "assets/5e9cda0d037c6b87b73250f9.zh.female.mp3",
+        "sound2": "assets/5e9cda0d037c6b87b73250f9.zh.male.mp3",
+        "sound3": "assets/5e9cda0d037c6b87b73250f9.en.mp3",
+        "pinyin": "é",
+        "english": "goose"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250fa.png",
+        "sound1": "assets/5e9cda0d037c6b87b73250fa.zh.female.mp3",
+        "sound2": "assets/5e9cda0d037c6b87b73250fa.zh.male.mp3",
+        "sound3": "assets/5e9cda0d037c6b87b73250fa.en.mp3",
+        "pinyin": "mǎ",
+        "english": "horse"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250fb.png",
+        "sound1": "assets/5e9cda0d037c6b87b73250fb.zh.female.mp3",
+        "sound2": "assets/5e9cda0d037c6b87b73250fb.zh.male.mp3",
+        "sound3": "assets/5e9cda0d037c6b87b73250fb.en.mp3",
+        "pinyin": "zhū",
+        "english": "pig"
+    },
+    {
+        "image": "assets/5e9cda0e037c6b87b73250fc.png",
+        "sound1": "assets/5e9cda0e037c6b87b73250fc.zh.female.mp3",
+        "sound2": "assets/5e9cda0e037c6b87b73250fc.zh.male.mp3",
+        "sound3": "assets/5e9cda0e037c6b87b73250fc.en.mp3",
+        "pinyin": "tù zi",
+        "english": "rabbit"
+    },
+    {
+        "image": "assets/5e9cda0e037c6b87b73250fd.png",
+        "sound1": "assets/5e9cda0e037c6b87b73250fd.zh.female.mp3",
+        "sound2": "assets/5e9cda0e037c6b87b73250fd.zh.male.mp3",
+        "sound3": "assets/5e9cda0e037c6b87b73250fd.en.mp3",
+        "pinyin": "lǘ",
+        "english": "donkey"
+    }
+];
