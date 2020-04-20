9fe94215b77fab79b13bc479ceb103c9f61e1c72
minzhang190 <minzhang190@gmail.com>
2020-04-20 00:30:55 +0000

    +20200419g4

---
+++ 20200419g4/data.js
@@ -0,0 +1,204 @@
+var title = "Jìjié, Yuèfèn Hé Xīngqī";
+
+var data = [
+    {
+        "image": "assets/5e9ccac2d50a14ecf823ab95.png",
+        "sound1": "assets/5e9ccac2d50a14ecf823ab95.zh.female.mp3",
+        "sound2": "assets/5e9ccac2d50a14ecf823ab95.zh.male.mp3",
+        "sound3": "assets/5e9ccac2d50a14ecf823ab95.en.mp3",
+        "pinyin": "jì jié",
+        "english": "seasons"
+    },
+    {
+        "image": "assets/5e9ccac3d50a14ecf823ab96.png",
+        "sound1": "assets/5e9ccac3d50a14ecf823ab96.zh.female.mp3",
+        "sound2": "assets/5e9ccac3d50a14ecf823ab96.zh.male.mp3",
+        "sound3": "assets/5e9ccac3d50a14ecf823ab96.en.mp3",
+        "pinyin": "chūn tiān",
+        "english": "spring"
+    },
+    {
+        "image": "assets/5e9ccac4d50a14ecf823ab97.png",
+        "sound1": "assets/5e9ccac4d50a14ecf823ab97.zh.female.mp3",
+        "sound2": "assets/5e9ccac4d50a14ecf823ab97.zh.male.mp3",
+        "sound3": "assets/5e9ccac4d50a14ecf823ab97.en.mp3",
+        "pinyin": "xià tiān",
+        "english": "summer"
+    },
+    {
+        "image": "assets/5e9ccac5d50a14ecf823ab98.png",
+        "sound1": "assets/5e9ccac5d50a14ecf823ab98.zh.female.mp3",
+        "sound2": "assets/5e9ccac5d50a14ecf823ab98.zh.male.mp3",
+        "sound3": "assets/5e9ccac5d50a14ecf823ab98.en.mp3",
+        "pinyin": "qiū tiān",
+        "english": "autumn"
+    },
+    {
+        "image": "assets/5e9ccac6d50a14ecf823ab99.png",
+        "sound1": "assets/5e9ccac6d50a14ecf823ab99.zh.female.mp3",
+        "sound2": "assets/5e9ccac6d50a14ecf823ab99.zh.male.mp3",
+        "sound3": "assets/5e9ccac6d50a14ecf823ab99.en.mp3",
+        "pinyin": "dōng tiān",
+        "english": "winter"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d5.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03d5.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03d5.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03d5.en.mp3",
+        "pinyin": "yī yuè",
+        "english": "January"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d6.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03d6.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03d6.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03d6.en.mp3",
+        "pinyin": "èr yuè",
+        "english": "February"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d7.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03d7.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03d7.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03d7.en.mp3",
+        "pinyin": "sān yuè",
+        "english": "March"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d8.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03d8.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03d8.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03d8.en.mp3",
+        "pinyin": "sì yuè",
+        "english": "April"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d9.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03d9.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03d9.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03d9.en.mp3",
+        "pinyin": "wǔ yuè",
+        "english": "May"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03da.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03da.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03da.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03da.en.mp3",
+        "pinyin": "liù yuè",
+        "english": "June"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03db.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03db.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03db.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03db.en.mp3",
+        "pinyin": "qī yuè",
+        "english": "July"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03dc.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03dc.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03dc.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03dc.en.mp3",
+        "pinyin": "bā yuè",
+        "english": "August"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03dd.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03dd.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03dd.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03dd.en.mp3",
+        "pinyin": "jiǔ yuè",
+        "english": "September"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03de.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03de.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03de.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03de.en.mp3",
+        "pinyin": "shí yuè",
+        "english": "October"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03df.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03df.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03df.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03df.en.mp3",
+        "pinyin": "shí yī yuè",
+        "english": "November"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03e0.png",
+        "sound1": "assets/5e8dae7caf74119e4f2c03e0.zh.female.mp3",
+        "sound2": "assets/5e8dae7caf74119e4f2c03e0.zh.male.mp3",
+        "sound3": "assets/5e8dae7caf74119e4f2c03e0.en.mp3",
+        "pinyin": "shí èr yuè",
+        "english": "December"
+    },
+    {
+        "image": "assets/5e9cd58217c76e7d6c69f908.png",
+        "sound1": "assets/5e9cd58217c76e7d6c69f908.zh.female.mp3",
+        "sound2": "assets/5e9cd58217c76e7d6c69f908.zh.male.mp3",
+        "sound3": "assets/5e9cd58217c76e7d6c69f908.en.mp3",
+        "pinyin": "xīng qī",
+        "english": "week"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dbf.png",
+        "sound1": "assets/5e8dad1da5b7500807899dbf.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dbf.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dbf.en.mp3",
+        "pinyin": "xīng qī yī",
+        "english": "Monday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc0.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc0.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc0.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc0.en.mp3",
+        "pinyin": "xīng qī èr",
+        "english": "Tuesday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc1.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc1.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc1.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc1.en.mp3",
+        "pinyin": "xīng qī sān",
+        "english": "Wednesday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc2.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc2.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc2.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc2.en.mp3",
+        "pinyin": "xīng qī sì",
+        "english": "Thursday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc3.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc3.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc3.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc3.en.mp3",
+        "pinyin": "xīng qī wǔ",
+        "english": "Friday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc4.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc4.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc4.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc4.en.mp3",
+        "pinyin": "xīng qī liù",
+        "english": "Saturday"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc5.png",
+        "sound1": "assets/5e8dad1da5b7500807899dc5.zh.female.mp3",
+        "sound2": "assets/5e8dad1da5b7500807899dc5.zh.male.mp3",
+        "sound3": "assets/5e8dad1da5b7500807899dc5.en.mp3",
+        "pinyin": "xīng qī rì",
+        "english": "Sunday"
+    }
+];
