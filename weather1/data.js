d66401ba01fd6eaf85ad6a89e7f1b7b07957afda
minzhang190 <minzhang190@gmail.com>
2020-04-25 03:35:32 +0000

    +weather1/2

---
+++ weather1/data.js
@@ -0,0 +1,104 @@
+var title = "Tiānqì (1)";
+
+var titleColor = "#000000";
+var pinyinColor = "#000000";
+var questionColor = "#000000";
+
+var data = [
+    {
+        "image": "assets/5e9cd3b0c9c8767483d7747a.png",
+        "sound1": "assets/5e9cd3b0c9c8767483d7747a.zh.female.mp3",
+        "sound2": "assets/5e9cd3b0c9c8767483d7747a.zh.male.mp3",
+        "sound3": "assets/5e9cd3b0c9c8767483d7747a.en.mp3",
+        "pinyin": "tiān qì",
+        "english": "weather"
+    },
+    {
+        "image": "assets/5e9cd3b1c9c8767483d7747c.png",
+        "sound1": "assets/5e9cd3b1c9c8767483d7747c.zh.female.mp3",
+        "sound2": "assets/5e9cd3b1c9c8767483d7747c.zh.male.mp3",
+        "sound3": "assets/5e9cd3b1c9c8767483d7747c.en.mp3",
+        "pinyin": "tài yáng",
+        "english": "sun"
+    },
+    {
+        "image": "assets/5e9cd3b3c9c8767483d7747e.png",
+        "sound1": "assets/5e9cd3b3c9c8767483d7747e.zh.female.mp3",
+        "sound2": "assets/5e9cd3b3c9c8767483d7747e.zh.male.mp3",
+        "sound3": "assets/5e9cd3b3c9c8767483d7747e.en.mp3",
+        "pinyin": "qíng lǎng",
+        "english": "sunny"
+    },
+    {
+        "image": "assets/5e9cd3b3c9c8767483d7747f.png",
+        "sound1": "assets/5e9cd3b3c9c8767483d7747f.zh.female.mp3",
+        "sound2": "assets/5e9cd3b3c9c8767483d7747f.zh.male.mp3",
+        "sound3": "assets/5e9cd3b3c9c8767483d7747f.en.mp3",
+        "pinyin": "yún",
+        "english": "cloud"
+    },
+    {
+        "image": "assets/5e9cd3b5c9c8767483d77480.png",
+        "sound1": "assets/5e9cd3b5c9c8767483d77480.zh.female.mp3",
+        "sound2": "assets/5e9cd3b5c9c8767483d77480.zh.male.mp3",
+        "sound3": "assets/5e9cd3b5c9c8767483d77480.en.mp3",
+        "pinyin": "fēng",
+        "english": "wind"
+    },
+    {
+        "image": "assets/5e9cd3b6c9c8767483d77481.png",
+        "sound1": "assets/5e9cd3b6c9c8767483d77481.zh.female.mp3",
+        "sound2": "assets/5e9cd3b6c9c8767483d77481.zh.male.mp3",
+        "sound3": "assets/5e9cd3b6c9c8767483d77481.en.mp3",
+        "pinyin": "yǔ",
+        "english": "rain"
+    },
+    {
+        "image": "assets/5e9cd3b7c9c8767483d77482.png",
+        "sound1": "assets/5e9cd3b7c9c8767483d77482.zh.female.mp3",
+        "sound2": "assets/5e9cd3b7c9c8767483d77482.zh.male.mp3",
+        "sound3": "assets/5e9cd3b7c9c8767483d77482.en.mp3",
+        "pinyin": "xuě",
+        "english": "snow"
+    },
+    {
+        "image": "assets/5e9cd3b9c9c8767483d77483.png",
+        "sound1": "assets/5e9cd3b9c9c8767483d77483.zh.female.mp3",
+        "sound2": "assets/5e9cd3b9c9c8767483d77483.zh.male.mp3",
+        "sound3": "assets/5e9cd3b9c9c8767483d77483.en.mp3",
+        "pinyin": "dǎ léi",
+        "english": "thundering"
+    },
+    {
+        "image": "assets/5e9cd3bac9c8767483d77484.png",
+        "sound1": "assets/5e9cd3bac9c8767483d77484.zh.female.mp3",
+        "sound2": "assets/5e9cd3bac9c8767483d77484.zh.male.mp3",
+        "sound3": "assets/5e9cd3bac9c8767483d77484.en.mp3",
+        "pinyin": "shǎn diàn",
+        "english": "lightning"
+    },
+    {
+        "image": "assets/5e9cd3bbc9c8767483d77485.png",
+        "sound1": "assets/5e9cd3bbc9c8767483d77485.zh.female.mp3",
+        "sound2": "assets/5e9cd3bbc9c8767483d77485.zh.male.mp3",
+        "sound3": "assets/5e9cd3bbc9c8767483d77485.en.mp3",
+        "pinyin": "léi zhèn yǔ",
+        "english": "thunderstorm"
+    },
+    {
+        "image": "assets/5e9cd3bcc9c8767483d77486.png",
+        "sound1": "assets/5e9cd3bcc9c8767483d77486.zh.female.mp3",
+        "sound2": "assets/5e9cd3bcc9c8767483d77486.zh.male.mp3",
+        "sound3": "assets/5e9cd3bcc9c8767483d77486.en.mp3",
+        "pinyin": "qǐ wù",
+        "english": "foggy"
+    },
+    {
+        "image": "assets/5e9cd3bcc9c8767483d77487.png",
+        "sound1": "assets/5e9cd3bcc9c8767483d77487.zh.female.mp3",
+        "sound2": "assets/5e9cd3bcc9c8767483d77487.zh.male.mp3",
+        "sound3": "assets/5e9cd3bcc9c8767483d77487.en.mp3",
+        "pinyin": "cǎi hóng",
+        "english": "rainbow"
+    }
+];
---
+++ weather2/data.js
@@ -0,0 +1,104 @@
+var title = "Tiānqì (2)";
+
+var titleColor = "#000000";
+var pinyinColor = "#000000";
+var questionColor = "#000000";
+
+var data = [
+    {
+        "image": "assets/5e9cd3b0c9c8767483d7747b.png",
+        "sound1": "assets/5e9cd3b0c9c8767483d7747b.zh.female.mp3",
+        "sound2": "assets/5e9cd3b0c9c8767483d7747b.zh.male.mp3",
+        "sound3": "assets/5e9cd3b0c9c8767483d7747b.en.mp3",
+        "pinyin": "tiān qì yù bào",
+        "english": "weather forecast"
+    },
+    {
+        "image": "assets/5e9cd3b2c9c8767483d7747d.png",
+        "sound1": "assets/5e9cd3b2c9c8767483d7747d.zh.female.mp3",
+        "sound2": "assets/5e9cd3b2c9c8767483d7747d.zh.male.mp3",
+        "sound3": "assets/5e9cd3b2c9c8767483d7747d.en.mp3",
+        "pinyin": "tài yáng hěn dà",
+        "english": "The sun is big. (It's sunny.)"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f497.png",
+        "sound1": "assets/5e8db08a26af9d56f020f497.zh.female.mp3",
+        "sound2": "assets/5e8db08a26af9d56f020f497.zh.male.mp3",
+        "sound3": "assets/5e8db08a26af9d56f020f497.en.mp3",
+        "pinyin": "duō yún",
+        "english": "cloudy"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f494.png",
+        "sound1": "assets/5e8db08a26af9d56f020f494.zh.female.mp3",
+        "sound2": "assets/5e8db08a26af9d56f020f494.zh.male.mp3",
+        "sound3": "assets/5e8db08a26af9d56f020f494.en.mp3",
+        "pinyin": "guā fēng",
+        "english": "windy"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f495.png",
+        "sound1": "assets/5e8db08a26af9d56f020f495.zh.female.mp3",
+        "sound2": "assets/5e8db08a26af9d56f020f495.zh.male.mp3",
+        "sound3": "assets/5e8db08a26af9d56f020f495.en.mp3",
+        "pinyin": "xià yǔ",
+        "english": "rainy"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f496.png",
+        "sound1": "assets/5e8db08a26af9d56f020f496.zh.female.mp3",
+        "sound2": "assets/5e8db08a26af9d56f020f496.zh.male.mp3",
+        "sound3": "assets/5e8db08a26af9d56f020f496.en.mp3",
+        "pinyin": "xià xuě",
+        "english": "snowy"
+    },
+    {
+        "image": "assets/5e9cd3bdc9c8767483d77488.png",
+        "sound1": "assets/5e9cd3bdc9c8767483d77488.zh.female.mp3",
+        "sound2": "assets/5e9cd3bdc9c8767483d77488.zh.male.mp3",
+        "sound3": "assets/5e9cd3bdc9c8767483d77488.en.mp3",
+        "pinyin": "hěn rè",
+        "english": "very hot"
+    },
+    {
+        "image": "assets/5e9cd3bec9c8767483d77489.png",
+        "sound1": "assets/5e9cd3bec9c8767483d77489.zh.female.mp3",
+        "sound2": "assets/5e9cd3bec9c8767483d77489.zh.male.mp3",
+        "sound3": "assets/5e9cd3bec9c8767483d77489.en.mp3",
+        "pinyin": "hěn lěng",
+        "english": "very cold"
+    },
+    {
+        "image": "assets/5e9cd3bfc9c8767483d7748a.png",
+        "sound1": "assets/5e9cd3bfc9c8767483d7748a.zh.female.mp3",
+        "sound2": "assets/5e9cd3bfc9c8767483d7748a.zh.male.mp3",
+        "sound3": "assets/5e9cd3bfc9c8767483d7748a.en.mp3",
+        "pinyin": "nuǎn huo",
+        "english": "warm"
+    },
+    {
+        "image": "assets/5e9cd3c0c9c8767483d7748b.png",
+        "sound1": "assets/5e9cd3c0c9c8767483d7748b.zh.female.mp3",
+        "sound2": "assets/5e9cd3c0c9c8767483d7748b.zh.male.mp3",
+        "sound3": "assets/5e9cd3c0c9c8767483d7748b.en.mp3",
+        "pinyin": "liáng kuài",
+        "english": "cool"
+    },
+    {
+        "image": "assets/5e9cd3c1c9c8767483d7748c.png",
+        "sound1": "assets/5e9cd3c1c9c8767483d7748c.zh.female.mp3",
+        "sound2": "assets/5e9cd3c1c9c8767483d7748c.zh.male.mp3",
+        "sound3": "assets/5e9cd3c1c9c8767483d7748c.en.mp3",
+        "pinyin": "cháo shī",
+        "english": "humid"
+    },
+    {
+        "image": "assets/5e9cd3c2c9c8767483d7748d.png",
+        "sound1": "assets/5e9cd3c2c9c8767483d7748d.zh.female.mp3",
+        "sound2": "assets/5e9cd3c2c9c8767483d7748d.zh.male.mp3",
+        "sound3": "assets/5e9cd3c2c9c8767483d7748d.en.mp3",
+        "pinyin": "lóng juǎn fēng",
+        "english": "tornado"
+    }
+];
