4d4bdd50bb373492c6a6984bbd282fd579d1f020
minzhang190 <minzhang190@gmail.com>
2020-05-18 05:35:36 +0000

    +body1/2

---
+++ body1/data.js
@@ -0,0 +1,128 @@
+var title = "Shēntǐ (1)";
+
+var titleColor = "#000000";
+
+var questionLine = "Zhè shì shénme shēntǐ bùwèi?";
+
+var data = [
+    {
+        "image": "assets/5e943b2b25c505681d0f1dee.png",
+        "sound1": "assets/5e943b2b25c505681d0f1dee.zh.female.mp3",
+        "sound2": "assets/5e943b2b25c505681d0f1dee.zh.male.mp3",
+        "sound3": "assets/5e943b2b25c505681d0f1dee.en.mp3",
+        "pinyin": "shēn tǐ",
+        "english": "body"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430631.png",
+        "sound1": "assets/5e8dac932c02d5b61d430631.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430631.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430631.en.mp3",
+        "pinyin": "tóu fà",
+        "english": "hair"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063c.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063c.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063c.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063c.en.mp3",
+        "pinyin": "bó zi",
+        "english": "neck"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062e.png",
+        "sound1": "assets/5e8dac932c02d5b61d43062e.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43062e.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43062e.en.mp3",
+        "pinyin": "tóu",
+        "english": "head"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062f.png",
+        "sound1": "assets/5e8dac932c02d5b61d43062f.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43062f.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43062f.en.mp3",
+        "pinyin": "jiān bǎng",
+        "english": "shoulder"
+    },
+    {
+        "image": "assets/5e943b2d25c505681d0f1def.png",
+        "sound1": "assets/5e943b2d25c505681d0f1def.zh.female.mp3",
+        "sound2": "assets/5e943b2d25c505681d0f1def.zh.male.mp3",
+        "sound3": "assets/5e943b2d25c505681d0f1def.en.mp3",
+        "pinyin": "xiōng pú",
+        "english": "chest"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062d.png",
+        "sound1": "assets/5e8dac932c02d5b61d43062d.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43062d.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43062d.en.mp3",
+        "pinyin": "shǒu",
+        "english": "hand"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430630.png",
+        "sound1": "assets/5e8dac932c02d5b61d430630.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430630.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430630.en.mp3",
+        "pinyin": "shǒu zhǐ",
+        "english": "finger"
+    },
+    {
+        "image": "assets/5e943b2e25c505681d0f1df0.png",
+        "sound1": "assets/5e943b2e25c505681d0f1df0.zh.female.mp3",
+        "sound2": "assets/5e943b2e25c505681d0f1df0.zh.male.mp3",
+        "sound3": "assets/5e943b2e25c505681d0f1df0.en.mp3",
+        "pinyin": "jiǎo",
+        "english": "foot"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063d.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063d.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063d.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063d.en.mp3",
+        "pinyin": "jiǎo zhǐ",
+        "english": "toe"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063b.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063b.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063b.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063b.en.mp3",
+        "pinyin": "tuǐ",
+        "english": "leg"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430638.png",
+        "sound1": "assets/5e8dac932c02d5b61d430638.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430638.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430638.en.mp3",
+        "pinyin": "xī gài",
+        "english": "knee"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062c.png",
+        "sound1": "assets/5e8dac932c02d5b61d43062c.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43062c.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43062c.en.mp3",
+        "pinyin": "gē bo",
+        "english": "arm"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063f.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063f.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063f.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063f.en.mp3",
+        "pinyin": "gē bo zhǒu",
+        "english": "elbow"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430639.png",
+        "sound1": "assets/5e8dac932c02d5b61d430639.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430639.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430639.en.mp3",
+        "pinyin": "dù zi",
+        "english": "stomach"
+    }
+];
---
+++ body2/data.js
@@ -0,0 +1,128 @@
+var title = "Shēntǐ (2)";
+
+var titleColor = "#000000";
+
+var questionLine = "Zhè shì shénme shēntǐ bùwèi?";
+
+var data = [
+    {
+        "image": "assets/5e8dac932c02d5b61d43063a.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063a.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063a.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063a.en.mp3",
+        "pinyin": "yá chǐ",
+        "english": "teeth"
+    },
+    {
+        "image": "assets/5e943b3125c505681d0f1df1.png",
+        "sound1": "assets/5e943b3125c505681d0f1df1.zh.female.mp3",
+        "sound2": "assets/5e943b3125c505681d0f1df1.zh.male.mp3",
+        "sound3": "assets/5e943b3125c505681d0f1df1.en.mp3",
+        "pinyin": "zuǐ chún",
+        "english": "lips"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430636.png",
+        "sound1": "assets/5e8dac932c02d5b61d430636.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430636.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430636.en.mp3",
+        "pinyin": "zuǐ",
+        "english": "mouth"
+    },
+    {
+        "image": "assets/5e943b3225c505681d0f1df2.png",
+        "sound1": "assets/5e943b3225c505681d0f1df2.zh.female.mp3",
+        "sound2": "assets/5e943b3225c505681d0f1df2.zh.male.mp3",
+        "sound3": "assets/5e943b3225c505681d0f1df2.en.mp3",
+        "pinyin": "xià bā",
+        "english": "chin"
+    },
+    {
+        "image": "assets/5e943b3225c505681d0f1df3.png",
+        "sound1": "assets/5e943b3225c505681d0f1df3.zh.female.mp3",
+        "sound2": "assets/5e943b3225c505681d0f1df3.zh.male.mp3",
+        "sound3": "assets/5e943b3225c505681d0f1df3.en.mp3",
+        "pinyin": "qián é",
+        "english": "forehead"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430634.png",
+        "sound1": "assets/5e8dac932c02d5b61d430634.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430634.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430634.en.mp3",
+        "pinyin": "yǎn jīng",
+        "english": "eyes"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430633.png",
+        "sound1": "assets/5e8dac932c02d5b61d430633.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430633.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430633.en.mp3",
+        "pinyin": "ěr duō",
+        "english": "ear"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430635.png",
+        "sound1": "assets/5e8dac932c02d5b61d430635.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430635.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430635.en.mp3",
+        "pinyin": "bí zi",
+        "english": "nose"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430632.png",
+        "sound1": "assets/5e8dac932c02d5b61d430632.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430632.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430632.en.mp3",
+        "pinyin": "liǎn",
+        "english": "face"
+    },
+    {
+        "image": "assets/5e943b3425c505681d0f1df4.png",
+        "sound1": "assets/5e943b3425c505681d0f1df4.zh.female.mp3",
+        "sound2": "assets/5e943b3425c505681d0f1df4.zh.male.mp3",
+        "sound3": "assets/5e943b3425c505681d0f1df4.en.mp3",
+        "pinyin": "jiǎo huái",
+        "english": "ankle"
+    },
+    {
+        "image": "assets/5e943b3425c505681d0f1df5.png",
+        "sound1": "assets/5e943b3425c505681d0f1df5.zh.female.mp3",
+        "sound2": "assets/5e943b3425c505681d0f1df5.zh.male.mp3",
+        "sound3": "assets/5e943b3425c505681d0f1df5.en.mp3",
+        "pinyin": "hóu lóng",
+        "english": "throat"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430637.png",
+        "sound1": "assets/5e8dac932c02d5b61d430637.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d430637.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d430637.en.mp3",
+        "pinyin": "méi máo",
+        "english": "eyebrow"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063e.png",
+        "sound1": "assets/5e8dac932c02d5b61d43063e.zh.female.mp3",
+        "sound2": "assets/5e8dac932c02d5b61d43063e.zh.male.mp3",
+        "sound3": "assets/5e8dac932c02d5b61d43063e.en.mp3",
+        "pinyin": "hòu bèi",
+        "english": "back"
+    },
+    {
+        "image": "assets/5e943b3625c505681d0f1df6.png",
+        "sound1": "assets/5e943b3625c505681d0f1df6.zh.female.mp3",
+        "sound2": "assets/5e943b3625c505681d0f1df6.zh.male.mp3",
+        "sound3": "assets/5e943b3625c505681d0f1df6.en.mp3",
+        "pinyin": "shé tóu",
+        "english": "tongue"
+    },
+    {
+        "image": "assets/5e943b3625c505681d0f1df7.png",
+        "sound1": "assets/5e943b3625c505681d0f1df7.zh.female.mp3",
+        "sound2": "assets/5e943b3625c505681d0f1df7.zh.male.mp3",
+        "sound3": "assets/5e943b3625c505681d0f1df7.en.mp3",
+        "pinyin": "gǔ tóu",
+        "english": "bone"
+    }
+];
