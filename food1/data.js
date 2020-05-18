d5fd51855c588ee8c5365bd8711621eb18b6b566
minzhang190 <minzhang190@gmail.com>
2020-05-18 02:15:44 +0000

    +food1/2

---
+++ food1/data.js
@@ -0,0 +1,183 @@
+var title = "Shíwù (A)";
+
+var titleColor = "#000000";
+titleProps.y = 300;
+
+var data = [
+    {
+        "image": "assets/5ec1d0605f5aaa74c0065962.png",
+        "sound1": "assets/5ec1d0605f5aaa74c0065962.zh.female.mp3",
+        "sound2": "assets/5ec1d0605f5aaa74c0065962.zh.male.mp3",
+        "sound3": "assets/5ec1d0605f5aaa74c0065962.en.mp3",
+        "pinyin": "shí wù",
+        "english": "food"
+    },
+    {
+        "image": "assets/5ec1d0615f5aaa74c0065963.png",
+        "sound1": "assets/5ec1d0615f5aaa74c0065963.zh.female.mp3",
+        "sound2": "assets/5ec1d0615f5aaa74c0065963.zh.male.mp3",
+        "sound3": "assets/5ec1d0615f5aaa74c0065963.en.mp3",
+        "pinyin": "zhōng cān",
+        "english": "Chinese food"
+    },
+    {
+        "image": "assets/5ec1d0615f5aaa74c0065964.png",
+        "sound1": "assets/5ec1d0615f5aaa74c0065964.zh.female.mp3",
+        "sound2": "assets/5ec1d0615f5aaa74c0065964.zh.male.mp3",
+        "sound3": "assets/5ec1d0615f5aaa74c0065964.en.mp3",
+        "pinyin": "jiǎo zi",
+        "english": "dumpling"
+    },
+    {
+        "image": "assets/5ec1d0625f5aaa74c0065965.png",
+        "sound1": "assets/5ec1d0625f5aaa74c0065965.zh.female.mp3",
+        "sound2": "assets/5ec1d0625f5aaa74c0065965.zh.male.mp3",
+        "sound3": "assets/5ec1d0625f5aaa74c0065965.en.mp3",
+        "pinyin": "miàn tiáo",
+        "english": "noodles"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39b1.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39b1.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39b1.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39b1.en.mp3",
+        "pinyin": "mǐ fàn",
+        "english": "rice"
+    },
+    {
+        "image": "assets/5ec1d0635f5aaa74c0065966.png",
+        "sound1": "assets/5ec1d0635f5aaa74c0065966.zh.female.mp3",
+        "sound2": "assets/5ec1d0635f5aaa74c0065966.zh.male.mp3",
+        "sound3": "assets/5ec1d0635f5aaa74c0065966.en.mp3",
+        "pinyin": "chǎo miàn",
+        "english": "chow mein"
+    },
+    {
+        "image": "assets/5ec1d0635f5aaa74c0065967.png",
+        "sound1": "assets/5ec1d0635f5aaa74c0065967.zh.female.mp3",
+        "sound2": "assets/5ec1d0635f5aaa74c0065967.zh.male.mp3",
+        "sound3": "assets/5ec1d0635f5aaa74c0065967.en.mp3",
+        "pinyin": "chǎo cài",
+        "english": "stir-fried vegetables"
+    },
+    {
+        "image": "assets/5ec1d0645f5aaa74c0065968.png",
+        "sound1": "assets/5ec1d0645f5aaa74c0065968.zh.female.mp3",
+        "sound2": "assets/5ec1d0645f5aaa74c0065968.zh.male.mp3",
+        "sound3": "assets/5ec1d0645f5aaa74c0065968.en.mp3",
+        "pinyin": "diǎn xīn",
+        "english": "dim sum"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39ad.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39ad.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39ad.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39ad.en.mp3",
+        "pinyin": "niú nǎi",
+        "english": "milk"
+    },
+    {
+        "image": "assets/5ec1d06a5f5aaa74c0065969.png",
+        "sound1": "assets/5ec1d06a5f5aaa74c0065969.zh.female.mp3",
+        "sound2": "assets/5ec1d06a5f5aaa74c0065969.zh.male.mp3",
+        "sound3": "assets/5ec1d06a5f5aaa74c0065969.en.mp3",
+        "pinyin": "cǎo méi niú nǎi",
+        "english": "strawberry milk"
+    },
+    {
+        "image": "assets/5ec1d06b5f5aaa74c006596a.png",
+        "sound1": "assets/5ec1d06b5f5aaa74c006596a.zh.female.mp3",
+        "sound2": "assets/5ec1d06b5f5aaa74c006596a.zh.male.mp3",
+        "sound3": "assets/5ec1d06b5f5aaa74c006596a.en.mp3",
+        "pinyin": "huáng yóu",
+        "english": "butter"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39b4.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39b4.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39b4.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39b4.en.mp3",
+        "pinyin": "nǎi lào",
+        "english": "cheese"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39b3.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39b3.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39b3.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39b3.en.mp3",
+        "pinyin": "suān nǎi",
+        "english": "yogurt"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39ab.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39ab.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39ab.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39ab.en.mp3",
+        "pinyin": "miàn bāo",
+        "english": "bread"
+    },
+    {
+        "image": "assets/5ec1d0725f5aaa74c006596b.png",
+        "sound1": "assets/5ec1d0725f5aaa74c006596b.zh.female.mp3",
+        "sound2": "assets/5ec1d0725f5aaa74c006596b.zh.male.mp3",
+        "sound3": "assets/5ec1d0725f5aaa74c006596b.en.mp3",
+        "pinyin": "yàn mài zhōu",
+        "english": "oatmeal"
+    },
+    {
+        "image": "assets/5ec1d0725f5aaa74c006596c.png",
+        "sound1": "assets/5ec1d0725f5aaa74c006596c.zh.female.mp3",
+        "sound2": "assets/5ec1d0725f5aaa74c006596c.zh.male.mp3",
+        "sound3": "assets/5ec1d0725f5aaa74c006596c.en.mp3",
+        "pinyin": "sōng bǐng",
+        "english": "pancake"
+    },
+    {
+        "image": "assets/5ec1d0725f5aaa74c006596d.png",
+        "sound1": "assets/5ec1d0725f5aaa74c006596d.zh.female.mp3",
+        "sound2": "assets/5ec1d0725f5aaa74c006596d.zh.male.mp3",
+        "sound3": "assets/5ec1d0725f5aaa74c006596d.en.mp3",
+        "pinyin": "huá fū bǐng",
+        "english": "waffle"
+    },
+    {
+        "image": "assets/5ec1d0735f5aaa74c006596e.png",
+        "sound1": "assets/5ec1d0735f5aaa74c006596e.zh.female.mp3",
+        "sound2": "assets/5ec1d0735f5aaa74c006596e.zh.male.mp3",
+        "sound3": "assets/5ec1d0735f5aaa74c006596e.en.mp3",
+        "pinyin": "miàn fěn",
+        "english": "flour"
+    },
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39ac.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39ac.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39ac.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39ac.en.mp3",
+        "pinyin": "jī dàn",
+        "english": "egg"
+    },
+    {
+        "image": "assets/5ec1d0745f5aaa74c006596f.png",
+        "sound1": "assets/5ec1d0745f5aaa74c006596f.zh.female.mp3",
+        "sound2": "assets/5ec1d0745f5aaa74c006596f.zh.male.mp3",
+        "sound3": "assets/5ec1d0745f5aaa74c006596f.en.mp3",
+        "pinyin": "dàn huáng",
+        "english": "egg yolk"
+    },
+    {
+        "image": "assets/5ec1d0745f5aaa74c0065970.png",
+        "sound1": "assets/5ec1d0745f5aaa74c0065970.zh.female.mp3",
+        "sound2": "assets/5ec1d0745f5aaa74c0065970.zh.male.mp3",
+        "sound3": "assets/5ec1d0745f5aaa74c0065970.en.mp3",
+        "pinyin": "dàn qīng",
+        "english": "egg white"
+    },
+    {
+        "image": "assets/5ec1d0755f5aaa74c0065971.png",
+        "sound1": "assets/5ec1d0755f5aaa74c0065971.zh.female.mp3",
+        "sound2": "assets/5ec1d0755f5aaa74c0065971.zh.male.mp3",
+        "sound3": "assets/5ec1d0755f5aaa74c0065971.en.mp3",
+        "pinyin": "shòu sī",
+        "english": "sushi"
+    }
+];
---
+++ food2/data.js
@@ -0,0 +1,175 @@
+var title = "Shíwù (B)";
+
+var titleColor = "#000000";
+titleProps.y = 450;
+
+var data = [
+    {
+        "image": "assets/5e8dad947aa6d6552ebb39b0.png",
+        "sound1": "assets/5e8dad947aa6d6552ebb39b0.zh.female.mp3",
+        "sound2": "assets/5e8dad947aa6d6552ebb39b0.zh.male.mp3",
+        "sound3": "assets/5e8dad947aa6d6552ebb39b0.en.mp3",
+        "pinyin": "ròu",
+        "english": "meat"
+    },
+    {
+        "image": "assets/5ec1d0765f5aaa74c0065972.png",
+        "sound1": "assets/5ec1d0765f5aaa74c0065972.zh.female.mp3",
+        "sound2": "assets/5ec1d0765f5aaa74c0065972.zh.male.mp3",
+        "sound3": "assets/5ec1d0765f5aaa74c0065972.en.mp3",
+        "pinyin": "jī ròu",
+        "english": "chicken"
+    },
+    {
+        "image": "assets/5ec1d0765f5aaa74c0065973.png",
+        "sound1": "assets/5ec1d0765f5aaa74c0065973.zh.female.mp3",
+        "sound2": "assets/5ec1d0765f5aaa74c0065973.zh.male.mp3",
+        "sound3": "assets/5ec1d0765f5aaa74c0065973.en.mp3",
+        "pinyin": "jī tuǐ",
+        "english": "chicken leg"
+    },
+    {
+        "image": "assets/5ec1d0775f5aaa74c0065974.png",
+        "sound1": "assets/5ec1d0775f5aaa74c0065974.zh.female.mp3",
+        "sound2": "assets/5ec1d0775f5aaa74c0065974.zh.male.mp3",
+        "sound3": "assets/5ec1d0775f5aaa74c0065974.en.mp3",
+        "pinyin": "zhū ròu",
+        "english": "pork"
+    },
+    {
+        "image": "assets/5ec1d0775f5aaa74c0065975.png",
+        "sound1": "assets/5ec1d0775f5aaa74c0065975.zh.female.mp3",
+        "sound2": "assets/5ec1d0775f5aaa74c0065975.zh.male.mp3",
+        "sound3": "assets/5ec1d0775f5aaa74c0065975.en.mp3",
+        "pinyin": "niú ròu",
+        "english": "beef"
+    },
+    {
+        "image": "assets/5ec1d0775f5aaa74c0065976.png",
+        "sound1": "assets/5ec1d0775f5aaa74c0065976.zh.female.mp3",
+        "sound2": "assets/5ec1d0775f5aaa74c0065976.zh.male.mp3",
+        "sound3": "assets/5ec1d0775f5aaa74c0065976.en.mp3",
+        "pinyin": "yáng ròu",
+        "english": "lamb"
+    },
+    {
+        "image": "assets/5ec1d0785f5aaa74c0065977.png",
+        "sound1": "assets/5ec1d0785f5aaa74c0065977.zh.female.mp3",
+        "sound2": "assets/5ec1d0785f5aaa74c0065977.zh.male.mp3",
+        "sound3": "assets/5ec1d0785f5aaa74c0065977.en.mp3",
+        "pinyin": "yú ròu",
+        "english": "fish"
+    },
+    {
+        "image": "assets/5ec1d0785f5aaa74c0065978.png",
+        "sound1": "assets/5ec1d0785f5aaa74c0065978.zh.female.mp3",
+        "sound2": "assets/5ec1d0785f5aaa74c0065978.zh.male.mp3",
+        "sound3": "assets/5ec1d0785f5aaa74c0065978.en.mp3",
+        "pinyin": "sān wén yú",
+        "english": "salmon"
+    },
+    {
+        "image": "assets/5ec1d0795f5aaa74c0065979.png",
+        "sound1": "assets/5ec1d0795f5aaa74c0065979.zh.female.mp3",
+        "sound2": "assets/5ec1d0795f5aaa74c0065979.zh.male.mp3",
+        "sound3": "assets/5ec1d0795f5aaa74c0065979.en.mp3",
+        "pinyin": "xiāng cháng",
+        "english": "sausage"
+    },
+    {
+        "image": "assets/5ec1d0795f5aaa74c006597a.png",
+        "sound1": "assets/5ec1d0795f5aaa74c006597a.zh.female.mp3",
+        "sound2": "assets/5ec1d0795f5aaa74c006597a.zh.male.mp3",
+        "sound3": "assets/5ec1d0795f5aaa74c006597a.en.mp3",
+        "pinyin": "huǒ tuǐ",
+        "english": "ham"
+    },
+    {
+        "image": "assets/5ec1d07a5f5aaa74c006597b.png",
+        "sound1": "assets/5ec1d07a5f5aaa74c006597b.zh.female.mp3",
+        "sound2": "assets/5ec1d07a5f5aaa74c006597b.zh.male.mp3",
+        "sound3": "assets/5ec1d07a5f5aaa74c006597b.en.mp3",
+        "pinyin": "péi gēn",
+        "english": "bacon"
+    },
+    {
+        "image": "assets/5ec1d07a5f5aaa74c006597c.png",
+        "sound1": "assets/5ec1d07a5f5aaa74c006597c.zh.female.mp3",
+        "sound2": "assets/5ec1d07a5f5aaa74c006597c.zh.male.mp3",
+        "sound3": "assets/5ec1d07a5f5aaa74c006597c.en.mp3",
+        "pinyin": "sān míng zhì",
+        "english": "sandwich"
+    },
+    {
+        "image": "assets/5ec1d07b5f5aaa74c006597d.png",
+        "sound1": "assets/5ec1d07b5f5aaa74c006597d.zh.female.mp3",
+        "sound2": "assets/5ec1d07b5f5aaa74c006597d.zh.male.mp3",
+        "sound3": "assets/5ec1d07b5f5aaa74c006597d.en.mp3",
+        "pinyin": "pī sà bǐng",
+        "english": "pizza"
+    },
+    {
+        "image": "assets/5ec1d07b5f5aaa74c006597e.png",
+        "sound1": "assets/5ec1d07b5f5aaa74c006597e.zh.female.mp3",
+        "sound2": "assets/5ec1d07b5f5aaa74c006597e.zh.male.mp3",
+        "sound3": "assets/5ec1d07b5f5aaa74c006597e.en.mp3",
+        "pinyin": "zhá shǔ tiáo",
+        "english": "french fries"
+    },
+    {
+        "image": "assets/5ec1d07c5f5aaa74c006597f.png",
+        "sound1": "assets/5ec1d07c5f5aaa74c006597f.zh.female.mp3",
+        "sound2": "assets/5ec1d07c5f5aaa74c006597f.zh.male.mp3",
+        "sound3": "assets/5ec1d07c5f5aaa74c006597f.en.mp3",
+        "pinyin": "shā lā",
+        "english": "salad"
+    },
+    {
+        "image": "assets/5ec1d07c5f5aaa74c0065980.png",
+        "sound1": "assets/5ec1d07c5f5aaa74c0065980.zh.female.mp3",
+        "sound2": "assets/5ec1d07c5f5aaa74c0065980.zh.male.mp3",
+        "sound3": "assets/5ec1d07c5f5aaa74c0065980.en.mp3",
+        "pinyin": "tāng",
+        "english": "soup"
+    },
+    {
+        "image": "assets/5ec1d07d5f5aaa74c0065981.png",
+        "sound1": "assets/5ec1d07d5f5aaa74c0065981.zh.female.mp3",
+        "sound2": "assets/5ec1d07d5f5aaa74c0065981.zh.male.mp3",
+        "sound3": "assets/5ec1d07d5f5aaa74c0065981.en.mp3",
+        "pinyin": "rè gǒu",
+        "english": "hot dog"
+    },
+    {
+        "image": "assets/5ec1d07d5f5aaa74c0065982.png",
+        "sound1": "assets/5ec1d07d5f5aaa74c0065982.zh.female.mp3",
+        "sound2": "assets/5ec1d07d5f5aaa74c0065982.zh.male.mp3",
+        "sound3": "assets/5ec1d07d5f5aaa74c0065982.en.mp3",
+        "pinyin": "hàn bǎo bāo",
+        "english": "hamburger"
+    },
+    {
+        "image": "assets/5ec1d07e5f5aaa74c0065983.png",
+        "sound1": "assets/5ec1d07e5f5aaa74c0065983.zh.female.mp3",
+        "sound2": "assets/5ec1d07e5f5aaa74c0065983.zh.male.mp3",
+        "sound3": "assets/5ec1d07e5f5aaa74c0065983.en.mp3",
+        "pinyin": "zǎo cān",
+        "english": "breakfast"
+    },
+    {
+        "image": "assets/5ec1d07e5f5aaa74c0065984.png",
+        "sound1": "assets/5ec1d07e5f5aaa74c0065984.zh.female.mp3",
+        "sound2": "assets/5ec1d07e5f5aaa74c0065984.zh.male.mp3",
+        "sound3": "assets/5ec1d07e5f5aaa74c0065984.en.mp3",
+        "pinyin": "wǔ cān",
+        "english": "lunch"
+    },
+    {
+        "image": "assets/5ec1d07f5f5aaa74c0065985.png",
+        "sound1": "assets/5ec1d07f5f5aaa74c0065985.zh.female.mp3",
+        "sound2": "assets/5ec1d07f5f5aaa74c0065985.zh.male.mp3",
+        "sound3": "assets/5ec1d07f5f5aaa74c0065985.en.mp3",
+        "pinyin": "wǎn cān",
+        "english": "dinner"
+    }
+];
